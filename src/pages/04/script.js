8a186dcc2f2c60333a4e6a92990f0e1258d8ec2a
Theophil Dango <theophiljadango@gmail.com>
2025-11-03 23:14:47 +0600

    scroll

---
+++ src/pages/04/script.js
@@ -0,0 +1,30 @@
+const upBtn = document.querySelector(".upBtn")
+
+let isFloating = false
+
+window.addEventListener('scroll',()=>{
+    if( window.scrollY > 100){
+        upBtn.classList.add('show')
+          if(!isFloating){
+            gsap.to(upBtn, {
+                y: -10,
+                repeat: -1,
+                yoyo: true,
+                duration: 1.2,
+                ease: "sine.inOut"
+
+            })
+
+            isFloating = true
+        }
+    }else{
+        upBtn.classList.remove("show")
+    }
+
+    upBtn.addEventListener("click",()=>{
+        window.scrollTo(0,0)
+
+      
+    })
+
+})
