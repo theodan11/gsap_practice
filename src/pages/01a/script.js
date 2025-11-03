d7dee2cb8b777d67e3c47ffcc1e86c8c7a90cd93
Theophil Dango <theophiljadango@gmail.com>
2025-11-03 18:02:27 +0600

    few changes

--- src/pages/01a/script.js
+++ src/pages/01a/script.js
@@ -1,13 +1,20 @@
-gsap.to(".box1", {
+const reloadBtn = document.querySelector("#reloadBtn")
+
+let gsapController = gsap.to(".box1", {
     opacity: 1,
     scale: 1,
     duration: .3,
-    onComplete: ()=>{
-        gsap.to(".box1",{
+    onComplete: () => {
+        gsap.to(".box1", {
             y: 20,
-            repeat: -1, 
+            repeat: -1,
             yoyo: true,
             delay: .3
         })
     }
+})
+
+
+reloadBtn.addEventListener("click", () => {
+    gsapController.restart()
 })
